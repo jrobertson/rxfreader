d188e91911035aacad44ffa238c8c57e4f98e273
James Robertson <digital.robertson@gmail.com>
2022-02-18 22:15:57 +0000

    1st commit

---
+++ lib/rxfreader.rb
@@ -0,0 +1,93 @@
+#!/usr/bin/env ruby
+
+# file: rxfreader.rb
+
+
+require 'gpd-request'
+require 'rest-client'
+require 'drb_fileclient'
+
+
+# RXF originally stands for Read XML File.
+
+
+module RXFRead
+
+  class FileX
+
+    def self.read(x)
+      RXReader.read(x).first
+    end
+
+  end
+end
+
+
+class RXFReaderException < Exception
+end
+
+class RXFReader
+  using ColouredText
+
+  def self.read(x, h={})   
+    
+    opt = {debug: false, auto: false}.merge(h)
+    
+    debug = opt[:debug]
+
+    raise RXFReaderException, 'nil found, expected a string' if x.nil?
+
+    if x.strip[/^<(\?xml|[^\?])/] then
+      
+      [x, :xml]
+
+    elsif x.lines.length == 1 then
+
+      if x[/^https?:\/\//] then
+        
+        puts 'before GPDRequest'.info if debug
+        
+        r = if opt[:username] and opt[:password] then
+          GPDRequest.new(opt[:username], opt[:password]).get(x)
+        else
+          response = RestClient.get(x)
+        end
+        
+        case r.code
+        when '404'          
+          raise(RXFReaderException, "404 %s not found" % x)
+        when '401'          
+          raise(RXFReaderException, "401 %s unauthorized access" % x)
+        end
+        
+        [r.body, :url]
+        
+      elsif  x[/^dfs:\/\//] then
+        
+        r = DfsFile.read(x)
+        [r, :dfs]
+        
+
+      elsif x[/^file:\/\//] or File.exists?(x) then
+        
+        puts 'RXFHelper.read before File.read' if debug
+        contents = File.read(File.expand_path(x.sub(%r{^file://}, '')))
+        
+        [contents, :file]
+        
+      elsif x =~ /\s/
+        [x, :text]
+      elsif DfsFile.exists?(x)
+        [DfsFile.read(x), :dfs]
+      else
+        [x, :unknown]
+      end
+      
+    else
+
+      [x, :unknown]
+    end
+  end
+  
+end
+
