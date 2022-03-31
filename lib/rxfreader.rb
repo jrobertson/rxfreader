e3efa49ff51cde63633fea28440b01d3f94cfcfb
James Robertson <digital.robertson@gmail.com>
2022-03-31 22:27:38 +0100

    bug fix: Within method *reveal*, if a partial URL is revealed, the URL is now prepended with the original URL origin.

--- lib/rxfreader.rb
+++ lib/rxfreader.rb
@@ -149,3 +149,144 @@ class RXFReader
   end
 
 end
+module RXFRead
+
+  class FileX
+
+    def self.exists?(s)   RXFReader.exists?(s)     end
+    def self.filetype(s)  RXFReader.filetype(s)     end
+    def self.read(s)      RXFReader.read(s).first   end
+
+  end
+
+end
+
+
+class RXFReaderException < Exception
+end
+
+class RXFReader
+  using ColouredText
+
+  def self.exists?(filename)
+
+    type = self.filetype(filename)
+
+    filex = case type
+    when :file
+      File
+    when :dfs
+      DfsFile
+    else
+      nil
+    end
+
+    return nil unless filex
+
+    filex.exists? filename
+
+  end
+
+  def self.filetype(x)
+
+    return :string if x.lines.length > 1
+
+    case x
+    when /^https?:\/\//
+      :http
+    when /^dfs:\/\//
+      :dfs
+    when /^file:\/\//
+      :file
+    else
+
+      if File.exists?(x) then
+        :file
+      else
+        :text
+      end
+
+    end
+  end
+
+  def self.read(x, h={})
+
+    opt = {debug: false, auto: false}.merge(h)
+
+    debug = opt[:debug]
+
+    raise RXFReaderException, 'nil found, expected a string' if x.nil?
+
+    if x.class.to_s =~ /Rexle$/ then
+
+      [x.xml, :rexle]
+
+    elsif x.strip[/^<(\?xml|[^\?])/] then
+
+      [x, :xml]
+
+    elsif x.lines.length == 1 then
+
+      if x[/^https?:\/\//] then
+
+        puts 'before GPDRequest'.info if debug
+
+        r = if opt[:username] and opt[:password] then
+          GPDRequest.new(opt[:username], opt[:password]).get(x)
+        else
+          response = RestClient.get(x)
+        end
+
+        case r.code
+        when '404'
+          raise(RXFReaderException, "404 %s not found" % x)
+        when '401'
+          raise(RXFReaderException, "401 %s unauthorized access" % x)
+        end
+
+        [r.body, :url]
+
+      elsif  x[/^dfs:\/\//] then
+
+        r = DfsFile.read(x).force_encoding('UTF-8')
+        [r, :dfs]
+
+
+      elsif x[/^file:\/\//] or File.exists?(x) then
+
+        puts 'RXFHelper.read before File.read' if debug
+        contents = File.read(File.expand_path(x.sub(%r{^file://}, '')))
+
+        [contents, :file]
+
+      elsif x =~ /\s/
+        [x, :text]
+      elsif DfsFile.exists?(x)
+        [DfsFile.read(x).force_encoding('UTF-8'), :dfs]
+      else
+        [x, :unknown]
+      end
+
+    else
+
+      [x, :unknown]
+    end
+  end
+
+  def self.reveal(uri_str, a=[])
+
+    u = URI.parse(uri_str)
+    response = Net::HTTP.get_response(u)
+
+    url = case response
+      when Net::HTTPRedirection then response['location']
+    end
+
+    return a << uri_str if url.nil?
+    url.prepend u.origin if not url[/^http/]
+
+    reveal(url, a << uri_str)
+  end
+
+
+end
